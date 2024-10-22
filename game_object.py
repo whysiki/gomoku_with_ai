637ecfc6aaa483dd6e49039575a1df097e0437b2
whysiki <whysiki@proton.me>
2024-10-22 19:13:47 +0800

    对抗

--- game_object.py
+++ game_object.py
@@ -1,96 +1,158 @@
 from dataclasses import dataclass
 import numpy as np
 from loguru import logger
+import random
+
+
+@dataclass
+class PlayerColor:
+    BLACK = "black"
+    WHITE = "white"
+
+
+@dataclass
+class PlayerType:
+    AI = "ai"
+    HUMAN = "human"
 
 
 @dataclass
 class Player:
     name: str
     color: str
+    type: str
 
-    def __init__(self, name: str, color: str):
-        if color.strip() not in ["black", "white"]:
+    def __init__(self, name: str, color: str, type: str):
+        if color.strip() not in (PlayerColor.BLACK, PlayerColor.WHITE):
             raise ValueError("Invalid color: {}, must be black or white".format(color))
         self.name = name
         self.color = color
+        if type.strip() not in (PlayerType.AI, PlayerType.HUMAN):
+            raise ValueError("Invalid type: {}, must be ai or human".format(type))
+        self.type = type
+
+    def test_ai_get_action(self, status_matrix: np.ndarray) -> tuple[int, int]:
+        # return random.choice(np.argwhere(status_matrix == 0))
+        # assert status_matrix.shape == (15, 15)
+        if self.type == PlayerType.AI:
+            return random.choice(np.argwhere(status_matrix == 0))
 
 
 class GomokuBoard:
     def __init__(self, canvas):
-        self.canvas = canvas
-        self.canvas.configure(bg="burlywood")
-        self.canvas.pack()
+        self.__canvas = canvas
+        self.__canvas__width = self.__canvas.winfo_width()
+        self.__canvas__height = self.__canvas.winfo_height()
+        self.board_size = 15
+        assert (
+            self.__canvas__width == self.__canvas__height == 640
+        ), "当前棋盘canvas大小必须为 640x640"
+        self.__canvas.configure(bg="burlywood")
+        self.__canvas.pack()
         self.draw_board()
-        self.canvas.bind("<Button-1>", self.place_white_piece)
-        self.status_matrix = np.zeros(
-            (15, 15), dtype=int
+        self.__status_matrix = np.zeros(
+            (self.board_size, self.board_size), dtype=int
         )  # 0: empty, 1: white, -1: black
         self.winner: Player = None
+        self.current_player: Player = None
+        self.last_player: Player = None
+        self.action_done = False
         logger.info("初始化棋盘完成, 接下来请设置当前玩家")
 
-    def set_current_player(self, curent_player: Player):
-        self.current_player = curent_player
-        logger.info(f"当前玩家: {self.current_player}")
+    @property
+    def canvas(self):
+        return self.__canvas
 
-    def reset_status_matrix(self):
-        self.status_matrix = np.zeros((15, 15), dtype=int)
+    @property
+    def status_matrix(self):
+        return self.__status_matrix
+
+    @property
+    def grid_size(self):
+        return self.__canvas__width // (self.board_size + 1)
+        # return 40
+
+    @property
+    def offset(self):
+        # return self.grid_size // 2
+        # return 40
+        return self.grid_size
+
+    def set_current_player(self, curent_player: Player):
+        if curent_player != self.last_player:  # 防止重复设置当前玩家
+            self.current_player = curent_player
+            self.action_done = False
+            if self.current_player.type == PlayerType.AI:
+                logger.info(
+                    f"当前玩家: {self.current_player}" + "AI 玩家, 请等待AI下棋"
+                )
+            elif self.current_player.type == PlayerType.HUMAN:
+                logger.info(
+                    f"当前玩家: {self.current_player}" + "人类玩家, 请点击棋盘下棋"
+                )
+                self.__canvas.bind("<Button-1>", self.__place_a_piece)
+            self.last_player = self.current_player
 
     def clear_board(self):
-        self.canvas.delete("all")
+        self.__canvas.delete("all")
         self.draw_board()
         self.reset_status_matrix()
         self.winner = None
+        self.action_done = False
+        self.current_player = None
+        self.last_player = None
         logger.info("棋盘已清空")
 
+    def reset_status_matrix(self):
+        self.__status_matrix = np.zeros((self.board_size, self.board_size), dtype=int)
+
     def update_status_matrix(self, x: int, y: int, color: str):
-        if color.strip() == "white".strip():
-            self.status_matrix[y][x] = 1
-        elif color.strip() == "black".strip():
-            self.status_matrix[y][x] = -1
+        """0: empty, 1: white, -1: black"""
+        if color.strip() == PlayerColor.WHITE:
+            self.__status_matrix[y][x] = 1
+        elif color.strip() == PlayerColor.BLACK:
+            self.__status_matrix[y][x] = -1
         else:
             raise ValueError("Invalid color: {}".format(color))
 
     def draw_board(self):
-        self.grid_size = 40
-        self.offset = 40
-        self.board_size = 15
         for i in range(self.board_size):
-            self.canvas.create_line(
+            self.__canvas.create_line(
                 self.offset,
                 self.offset + i * self.grid_size,
                 self.offset + (self.board_size - 1) * self.grid_size,
                 self.offset + i * self.grid_size,
                 fill="black",
             )
-            self.canvas.create_line(
+            self.__canvas.create_line(
                 self.offset + i * self.grid_size,
                 self.offset,
                 self.offset + i * self.grid_size,
                 self.offset + (self.board_size - 1) * self.grid_size,
                 fill="black",
             )
-            self.canvas.create_text(
+            self.__canvas.create_text(
                 self.offset - 20,
                 self.offset + i * self.grid_size,
                 text=str(i + 1),
                 anchor="e",
                 font=("Arial", 12),
             )
-            self.canvas.create_text(
+            self.__canvas.create_text(
                 self.offset + (self.board_size - 1) * self.grid_size + 20,
                 self.offset + i * self.grid_size,
                 text=str(i + 1),
                 anchor="w",
                 font=("Arial", 12),
             )
-            self.canvas.create_text(
+            self.__canvas.create_text(
                 self.offset + i * self.grid_size,
                 self.offset - 20,
                 text=chr(ord("A") + i),
                 anchor="s",
                 font=("Arial", 12),
             )
-            self.canvas.create_text(
+            self.__canvas.create_text(
                 self.offset + i * self.grid_size,
                 self.offset + (self.board_size - 1) * self.grid_size + 20,
                 text=chr(ord("A") + i),
@@ -103,27 +165,22 @@ class GomokuBoard:
     ) -> tuple[str, str]:
         return chr(ord("A") + index_x), str(index_y + 1)
 
-    def place_white_piece(self, event):
-        if self.winner:
-            logger.info(f"{self.winner} 已经胜利, 请重新开始游戏")
-            return
-        fill: str = self.current_player.color
-        x = (
-            round((event.x - self.offset) / self.grid_size) * self.grid_size
-            + self.offset
-        )
-        y = (
-            round((event.y - self.offset) / self.grid_size) * self.grid_size
-            + self.offset
-        )
+    def __caculate_place_oval_coordinate(
+        self, index_x: int, index_y: int
+    ) -> tuple[int, int]:
+        """从棋盘坐标转换为画布坐标"""
+        x = index_x * self.grid_size + self.offset
+        y = index_y * self.grid_size + self.offset
+        return x, y
 
-        index_x = round((x - self.offset) / self.grid_size)
-        index_y = round((y - self.offset) / self.grid_size)
-
+    def __place_oval(self, index_x: int, index_y: int):
+        """
+        由棋盘坐标放置棋子
+        """
         text_x, text_y = self.get_coordinate_text_for_index(index_x, index_y)
-
+        fill: str = self.current_player.color
         # 如果当前棋盘位置已经有棋子，则不再绘制
-        if self.status_matrix[index_y][index_x] != 0:
+        if self.__status_matrix[index_y][index_x] != 0:
             logger.debug(
                 "当前位置: {}={} 已经有棋子, 颜色: {}".format(
                     (index_x, index_y), (text_x, text_y), fill
@@ -131,7 +188,8 @@ class GomokuBoard:
             )
             return
         else:
-            self.canvas.create_oval(x - 15, y - 15, x + 15, y + 15, fill=fill)
+            x, y = self.__caculate_place_oval_coordinate(index_x, index_y)
+            self.__canvas.create_oval(x - 15, y - 15, x + 15, y + 15, fill=fill)
             logger.debug(
                 "当前位置: {}={} 绘制棋子完成, 颜色: {}".format(
                     (index_x, index_y),
@@ -141,12 +199,47 @@ class GomokuBoard:
             )
             self.update_status_matrix(index_x, index_y, fill)
 
-        if self.check_win(index_x, index_y):
+        if self.__check_win(index_x, index_y):
             logger.info(f"{self.current_player} 胜利")
             self.winner = self.current_player
 
-    def check_win(self, x, y):
-        color = self.status_matrix[y][x]
+    def action(self, index_x: int, index_y: int):
+        """执行下棋操作"""
+        if self.winner:
+            logger.info(f"{self.winner} 已经胜利, 请重新开始游戏")
+            return
+        self.__place_oval(index_x, index_y)
+        self.action_done = True
+
+    def __place_a_piece(self, event):
+        """点击棋盘下棋"""
+        if self.winner:
+            logger.info(f"{self.winner} 已经胜利, 请重新开始游戏")
+            return
+        if self.current_player:
+            if self.current_player.type == PlayerType.HUMAN:
+                x = (
+                    round((event.x - self.offset) / self.grid_size) * self.grid_size
+                    + self.offset
+                )
+                y = (
+                    round((event.y - self.offset) / self.grid_size) * self.grid_size
+                    + self.offset
+                )
+
+                index_x = round((x - self.offset) / self.grid_size)
+                index_y = round((y - self.offset) / self.grid_size)
+
+                self.action(index_x, index_y)
+
+            else:
+                logger.warning("当前玩家不是人类玩家, 请等待AI下棋")
+        else:
+            logger.warning("请先设置当前玩家")
+
+    def __check_win(self, index_x, index_y):
+        x, y = index_x, index_y
+        color = self.__status_matrix[y][x]
         directions = [
             (1, 0),  # 水平
             (0, 1),  # 垂直
@@ -160,7 +253,7 @@ class GomokuBoard:
                 if (
                     0 <= x + i * dx < self.board_size
                     and 0 <= y + i * dy < self.board_size
-                    and self.status_matrix[y + i * dy][x + i * dx] == color
+                    and self.__status_matrix[y + i * dy][x + i * dx] == color
                 ):
                     count += 1
                 else:
@@ -169,7 +262,7 @@ class GomokuBoard:
                 if (
                     0 <= x - i * dx < self.board_size
                     and 0 <= y - i * dy < self.board_size
-                    and self.status_matrix[y - i * dy][x - i * dx] == color
+                    and self.__status_matrix[y - i * dy][x - i * dx] == color
                 ):
                     count += 1
                 else:
--- gomoku_with_ai_cmd.py
+++ gomoku_with_ai_cmd.py
@@ -2,6 +2,7 @@
 import sys
 import os
 from os.path import abspath, dirname
+from tkinter import messagebox, simpledialog
 
 sys.path.insert(0, abspath(dirname(__file__)))
 import tkinter
@@ -9,7 +10,8 @@ from tkinter import *
 import Fun
 from rich import print
 from loguru import logger
-from game_object import Player, GomokuBoard
+from game_object import Player, GomokuBoard, PlayerColor, PlayerType
+import threading
 
 uiName = "gomoku_with_ai"
 ElementBGArray = {}
@@ -27,6 +29,7 @@ logger.add(
 )
 
 gbd = None
+is_priority = True
 
 
 # Form 'Form_1's Load Event :
@@ -37,11 +40,67 @@ def Form_1_onLoad(uiName, threadings=0):
     width = canvas.winfo_width()
     height = canvas.winfo_height()
     print(f"GomokuBoard , width: {width}, height: {height}")  # 640, 640
-    player1 = Player("player1", "white")
-    AI = Player("AI", "black")
-    gbd.set_current_player(player1)
 
 
 # Button 'Button_1' 's Command Event :
+# Restart Game
 def Button_1_onCommand(uiName, widgetName, threadings=0):
-    gbd.clear_board()
+    if gbd is not None:
+        gbd.clear_board()
+        threading.Thread(target=run_gbd, args=(is_priority,)).start()
+
+
+def run_gbd(is_priority):
+    player = Player(
+        name="Player",
+        color=PlayerColor.WHITE if is_priority else PlayerColor.BLACK,
+        type=PlayerType.HUMAN,
+    )
+    ai_player = Player(
+        name="AI",
+        color=PlayerColor.BLACK if is_priority else PlayerColor.WHITE,
+        type=PlayerType.AI,
+    )
+
+    def player_turn():
+        if gbd.winner:
+            return
+
+        gbd.set_current_player(player)
+        if not gbd.action_done:
+            gbd.canvas.after(100, player_turn)  # 继续等待玩家动作
+        else:
+            ai_turn()
+
+    def ai_turn():
+        if gbd.winner:
+            return
+        gbd.set_current_player(ai_player)
+        if not gbd.action_done:
+            status_matrix = gbd.status_matrix
+            ai_action = ai_player.test_ai_get_action(status_matrix)
+            gbd.action(*ai_action)
+            gbd.canvas.after(100, player_turn)  # 切换到玩家动作
+
+    if is_priority:
+        player_turn()
+    else:
+        ai_turn()
+
+
+# Button 'Button_2' 's Command Event :
+# Start Game
+def Button_2_onCommand(uiName, widgetName, threadings=0):
+    if gbd is not None:
+        # 选择先手
+        response = messagebox.askquestion(
+            "选择先手",
+            "是否选择先手？\n是 (Yes) 代表先手\n否 (No) 代表后手",
+            icon="question",
+            type="yesno",
+            default="yes",
+        )
+        is_priority = response == "yes"
+
+        # 开启线程
+        threading.Thread(target=run_gbd, args=(is_priority,)).start()
